a6cc23b793e665432fdedc8c0c3f41879941a7a7
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-06-19 09:22:20 +0500

    FOUNDATION: Impliment Bank Service And Validation All Methods

---
+++ Bank.Management.Console/Services/Foundations/Banks/BankBrokerStorageBroker.cs
@@ -0,0 +1,12 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+//----------------------------------------
+
+using Bank.Management.Console.Brokers.Storages.Bank;
+
+namespace Bank.Management.Console.Services.Foundations.Banks
+{
+    internal class BankBrokerStorageBroker : IBankStorageBroker
+    {
+    }
+}
